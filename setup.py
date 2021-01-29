77a6cbf2505e33974ed47a5d2b89f01c6b5e300d
Sebastian García Acosta <segaracos@outlook.com>
2021-01-29 12:58:30 -0500

    TF version ranged set

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import find_packages
 
 packages_list = [f"headlines_project.{s}" for s in find_packages(where="headlines_project")]
 
-#with open('requirements.txt') as f:
-#    required = f.read().splitlines()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(name='headlines_project',
       version='0.0.5',
       python_requires=">=3.6.0,<=3.8.0",
+      install_requires=required,
       description='Virality in headlines_project headlines_project',
       packages=packages_list,
       zip_safe=False)
